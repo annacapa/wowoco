e6fa16deba4cf9d38b64bb8441c39ac63c16ba50
anacapa <annadonlin@gmail.com>
2018-03-16 13:26:10 -0700

    This is the start of the questionnaire exercise.

---
+++ Pages/Form.cshtml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ecard.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ecard.Pages
+{
+    public class FormModel : PageModel
+    {
+
+        // WOWOCO: 1
+        [BindProperty]
+        public Greetings _myGreetings { get; set; }
+
+        // WOWOCO: 2
+        private DbBridge _myDbBridge { get; set; }
+
+        // WOWOCO: 3
+        private IConfiguration _myConfiguration { get; set; }
+
+        // WOWOCO: 4
+        public FormModel(DbBridge DbBridge, IConfiguration Configuration)
+        {
+            _myDbBridge = DbBridge;
+            _myConfiguration = Configuration;
+
+        }
+
+        public void OnGet() { }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPost()
+        {
+
+            if (await isValid())
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        _myGreetings.created = DateTime.Now.ToString();
+                        _myGreetings.created_ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                        // Power of replacing inputs before they are entered into the database
+                        _myGreetings.friendname = _myGreetings.friendname.Replace("i", "3");
+                        _myGreetings.friendname = _myGreetings.friendname.Replace("\"", "&quot;");
+                        _myGreetings.friendname = _myGreetings.friendname.Replace("She said, \"Hello!\"", "");
+                        _myGreetings.senderemail = _myGreetings.senderemail.ToLowerInvariant();
+                        _myGreetings.friendemail = _myGreetings.friendemail.ToUpperInvariant();
+
+                        // DB Related add record
+                        _myDbBridge.Greetings.Add(_myGreetings);
+                        _myDbBridge.SaveChanges();
+
+                        //REDIRECT to the page with a new operator (name/value pair)
+                        return RedirectToPage("Form", new { id = _myGreetings.ID });
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        return RedirectToPage("Form");
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("_myGreetings.reCaptcha", "Please verify you're not a robot!");
+            }
+
+            return Page();
+
+        }
+
+        /**
+         * reCAPTHCA SERVER SIDE VALIDATION 
+         * 
+         *      Create an HttpClient and store the the secret/response pair
+         *      Await for the sever to return a json obect 
+         * */
+        private async Task<bool> isValid()
+        {
+            var response = this.HttpContext.Request.Form["g-recaptcha-response"];
+            if (string.IsNullOrEmpty(response))
+                return false;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var values = new Dictionary<string, string>();
+                    //values.Add("secret", "6LfVpjEUAAAAAK0FdygAgh0P1gZ8QU24ildwT86r");
+                    values.Add("secret", _myConfiguration["ReCaptcha:PrivateKey"]);
+
+                    values.Add("response", response);
+                    //values.Add("remoteip", this.HttpContext.Connection.RemoteIpAddress.ToString()); 
+
+                    var query = new FormUrlEncodedContent(values);
+
+                    var post = client.PostAsync("https://www.google.com/recaptcha/api/siteverify", query);
+
+                    var json = await post.Result.Content.ReadAsStringAsync();
+
+                    if (json == null)
+                        return false;
+
+                    var results = JsonConvert.DeserializeObject<dynamic>(json);
+
+                    return results.success;
+                }
+
+            }
+            catch { }
+
+            return false;
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ecard.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +24,9 @@ namespace ecard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //WOWOCO
+            //register the database
+            services.AddDbContext<DbBridge>(options => options.UseSqlite(Configuration["MyDB"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,22 @@ namespace ecard
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+
+
+            //WOWOCO: Codeblock to auto-create the database when needed
+            using (var serviceScope = app
+                   .ApplicationServices
+                   .GetRequiredService<IServiceScopeFactory>()
+                   .CreateScope())
+            {
+                serviceScope
+                    .ServiceProvider
+                    .GetService<DbBridge>()
+                    .Database
+                    .EnsureCreated();
+            }
+                        
+
         }
     }
 }
